f536da07fb3996f6cca602b525d2d7c8e54254a6
unknown <danielchibuikem@gmail.com>
2022-11-15 16:17:04 +0700

    record with the hosted api

--- packages/frontend/src/pages/Track/TrackPage.tsx
+++ packages/frontend/src/pages/Track/TrackPage.tsx
@@ -36,7 +36,7 @@ const TrackPage = (props: any) => {
         notion.unsubscribe();
         console.log('the tracked data is', trackedData);
         // hard coded saving
-        axios.post("localhost:5676", trackedData)
+        axios.post("https://metawise-sample-backend.vercel.app/api/v1/powerband/save", trackedData)
         .then(res => toast("Data saved successfully"))
         .catch(err => toast.error("Data failed to save"))
     }
